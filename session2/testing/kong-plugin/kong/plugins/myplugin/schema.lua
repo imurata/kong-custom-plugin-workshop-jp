22eea41a2fad18edcffa984c4c74423be3071adf
imurata <61151479+imurata@users.noreply.github.com>
2025-04-10 17:17:10 +0900

    Update schema.lua

--- session2/testing/kong-plugin/kong/plugins/myplugin/schema.lua
+++ session2/testing/kong-plugin/kong/plugins/myplugin/schema.lua
@@ -1,45 +1,46 @@
 local typedefs = require "kong.db.schema.typedefs"
 
--- Grab pluginname from module name
+-- module名からプラグイン名を取得
 local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
 
 local schema = {
   name = plugin_name,
   fields = {
-    -- the 'fields' array is the top-level entry with fields defined by Kong
-    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
+    -- 'fields' 配列は、Kong によって定義された最上位のフィールドエントリ
+    { consumer = typedefs.no_consumer },  -- このプラグインはコンシューマ単位で設定できない（認証プラグインでは一般的）
     { protocols = typedefs.protocols_http },
     { config = {
-        -- The 'config' record is the custom part of the plugin schema
+        -- 'config' レコードは、プラグインスキーマ内のカスタム部分
         type = "record",
         fields = {
-          -- a standard defined field (typedef), with some customizations
+          -- 一部カスタマイズされた、定義済みの標準フィールド（typedef）
           { request_header = typedefs.header_name {
               required = true,
               default = "Hello-World" } },
           { response_header = typedefs.header_name {
               required = true,
               default = "Bye-World" } },
-          { ttl = { -- self defined field
+          { ttl = { -- 独自定義のフィールド
               type = "integer",
               default = 600,
               required = true,
-              gt = 0, }}, -- adding a constraint for the value
+              gt = 0, }}, -- 値に制約を追加
 
-          { remove_request_headers = { -- adding new field
+          { remove_request_headers = { -- 新しいフィールドを追加
             type = "array",
             elements = typedefs.header_name}},
         },
         entity_checks = {
-          -- add some validation rules across fields
-          -- the following is silly because it is always true, since they are both required
+          -- 複数のフィールドにまたがるバリデーションルールを追加
+          -- 以下のルールは常に真になるため無意味（両方必須なので）
           { at_least_one_of = { "request_header", "response_header" }, },
-          -- We specify that both header-names cannot be the same
+          -- 両方のヘッダー名が同じであってはならないというルールを指定
           { distinct = { "request_header", "response_header"} },
         },
       },
     },
   },
 }
+
 
 return schema
